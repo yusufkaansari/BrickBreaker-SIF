d263dd1a5569fe5fb505764ee6fe4198d0212625
Yusuf Kaan SARI <yusufkaansari@hotmail.com>
2023-09-17 19:20:14 +0300

    Adding the Paddle

---
+++ BrickBreakerSIF/Assets/GameFolder/Scripts/Paddle.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    float maxX, height, width, paddleWidth;
+    private void Awake()
+    {
+        height = Camera.main.orthographicSize;
+        width = height * Camera.main.aspect;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleWidth = GetComponent<SpriteRenderer>().bounds.extents.x;
+        maxX = width - paddleWidth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * horizontal * Time.deltaTime * speed);
+
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, -maxX, maxX);
+        transform.position = pos;
+    }
+}
